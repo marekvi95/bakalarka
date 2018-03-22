234d9d47daab06eaa3f51e628181504785d6fee7
Marek Vitula <mv.marekvitula@gmail.com>
2018-03-22 18:34:35 +0100

    up

--- motion_detect.py
+++ motion_detect.py
@@ -8,6 +8,21 @@ import config
 from config import BaseConfig
 from config import UserConfig
 
+#Setup logging
+logging.basicConfig(filename='logfile.log',level=logging.DEBUG,
+                    format='%(asctime)s %(threadName)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',)
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+logging.debug('Libraries loaded')
 
 class MotionAnalysis(picamera.array.PiMotionAnalysis):
     def __init__(self, camera, handler):

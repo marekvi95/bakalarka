de8ac64877745602075078daec9153e38f477f95
Marek Vitula <mv.marekvitula@gmail.com>
2018-03-26 19:01:04 +0200

    tst

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         #initialization of file uploader
         w = FileUploader(storage=UserConfig.storage, q=q)
         w.start()
-
+        logging.debug('File uploader started')
         #initialization daylight detection threadName
 
         #initialization of motion detection analysis

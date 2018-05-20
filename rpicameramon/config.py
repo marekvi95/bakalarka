67bddfb7db4c1b70a73cc339e374ac806057a339
Marek Vitula <marekvitula@Mareks-MacBook.local>
2018-05-20 17:46:41 +0200

    updated config

--- rpicameramon/config.py
+++ rpicameramon/config.py
@@ -5,29 +5,30 @@ import json
 class BaseConfig:
     """ Basic configuration which cannot be changed online
 
-        Variables:
+        Attributes:
+            imageDir (str):default image directory
+            imagePath (str): path to the directory + imageDir
+            imageWidth (int): width of taken pictures in pixels
+            imageHeight (int): height of taken pictures in pixels
+            imageVFlip (bool): flips image vertically if true
+            imageHFlip (bool): flips image Horizontally if true
+            imagePreview (bool): opens window with picture if true
+            imageQuality (int): JPEG quality 0...100
 
-        confFileName -- name of the default JSON configuration file
+            vecMagnitude (int): minimum magnitude of a motion vector
+            vecCount (int): minimal number of motion vectors
+            cameraFPS (int): framerate of the camera
 
-        imageDir -- default image directory
-        imagePath -- path to the directory + imageDir
-        imageNamePrefix -- Prefix for all image file names. Eg front-
-        imageWidth -- width of taken pictures in pixels
-        imageHeight -- height of taken pictures in pixels
-        imageVFlip -- flips image vertically if true
-        imageHFlip -- flips image Horizontally if true
-        imagePreview -- opens window with picture if true
-        imageQuality -- JPEG quality 0...100
+            confCheckTime (int): how long does it take between conf file checks in sec
 
-        confCheckTime -- how long does it take between conf file checks in sec
-
+            PIRpin (int): GPIO pin (BCM) where is PIR sensor connected
+            LEDpin (int): GPIO pin (BCM) where is LED connected
+            
     """
     # Default Settings
-    confFileName = "conf.json"
 
     imageDir = "images"
     imagePath = "/home/pi/" + imageDir
-    imageNamePrefix = 'capture-'  # Prefix for all image file names. Eg front-
     imageWidth = 640
     imageHeight = 368
     imageVFlip = False   # Flip image Vertically
@@ -39,13 +40,6 @@ class BaseConfig:
     vecMagnitude = 40  # Vector magnitude
     vecCount = 40  # How many vectors change
     cameraFPS = 10 # framerate
-
-    nightISO = 800
-    # nightShutSpeed = 2 * SECONDS2MICRO  # seconds times conversion to microseconds constant
-
-    # Advanced Settings not normally changed
-    testWidth = 128
-    testHeight = 80
 
     confCheckTime = 10
 
@@ -61,7 +55,6 @@ class BaseConfig:
 
     # File uploader sleep time in seconds
     fileUploadSleep = 5
-
     batchUploadWindow = 23
 
     GSMport = '/dev/ttyGSM1'
